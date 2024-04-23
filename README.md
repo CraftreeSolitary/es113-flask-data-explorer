460a508aa31b0e93bfdbcb6ca377e12f2049f9cc
Devansh Lodha <135693534+CraftreeSolitary@users.noreply.github.com>
2024-04-24 05:09:01 +0530

    readme

--- README.md
+++ README.md
@@ -1,1 +1,118 @@
 # es113-flask-data-explorer
+
+## Answers to all questions asked in 1e
+### 1e1
+Implement a robust search functionality that allows users to quickly
+search for specific records based on Bond Number or filter data based on any column in the table except Sr. No. and Status (e.g., date, political party, company name). The output should be displayed in the form of a table which is fetched from the database for a given query.
+#### Steps
+1. Go to Search Tab 
+![1e1 Step1](images/1e1(1).png)
+2. Enter Bond Number and Select Details to Display 
+![1e1 Step2](images/1e1(2).png)
+3. Click on Submit, to get the results
+![1e1 Step3](images/1e1(3).png)
+
+### 1e2
+The option to select a Company/Individual from a drop-down/search, and show how many bonds and the total value of bonds purchased per year. You can present a bar plot depicting your results.
+#### Steps
+1. Go to Company/Individual Stats Tab 
+![1e2 Step1](images/1e2(1).png)
+2. Enter a Company Name
+![1e2 Step2](images/1e2(2).png)
+3. Click on submit to get the results
+![1e2 Step3](images/1e2(3).png)
+
+### 1e3
+The option to select a political party from a drop-down/search, and show how many bonds and total value per year are in the timeline. You can present a bar plot depicting your results.
+#### Steps
+1. Go to Political Party Stats
+![1e3 Step1](images/1e3(1).png)
+2. Enter name of a Political Party
+![1e3 Step2](images/1e3(2).png)
+3. Click on Submit to get the results
+![1e3 Step3](images/1e3(3).png)
+
+### 1e4
+The option to select a political party from a drop-down/search, shows which companies have donated to it and what amount individually and combined.
+#### Steps
+1. Go to the Party Donation Stats Page
+![1e4 Step1](images/1e4(1).png)
+2. Enter name of a Political Party
+![1e4 Step2](images/1e4(2).png)
+3. Click on Submit to get the results
+![1e4 Step3](images/1e4(3).png)
+_output is truncated_
+
+### 1e5
+Similarly, provide an option to select a company from a drop-down/search, showcasing which parties they have donated and what amount individually and combined.
+#### Steps
+1. Go to the Company/Individual Donation Stats Page
+![1e5 Step1](images/1e5(1).png)
+2. Enter Company/Individual Name
+![1e5 Step2](images/1e5(2).png)
+3. Click on Submit to get the results
+![1e5 Step3](images/1e5(3).png)
+
+### 1e6
+Pie Charts are displayed
+
+## Downloadable Plots!
+1. Generate a chart on any relevant page
+![down step1](images/down1.png)
+2. Click on the Download Chart button and the download window on your system will pop up
+![down step1](images/down2.png)
+3. Save to any directory and open the png
+![down step1](images/chart(2).png)
+## Instructions to set up the website locally
+1. Download the zip file of this repository
+2. Unzip it to a folder
+3. Open Visual Studio Code
+4. Open the previously unzipped folder in VSCode
+5. Open the terminal (powershell for windows) and enter the following to set up a virtual environment:
+```bash
+python -m venv flask_env
+```
+```bash
+./flask_env/Scripts/activate.ps1
+```
+6. Install the dependencies in the virtual environment
+```bash
+pip install flask flask-mysqldb wtforms pyyaml
+```
+7. Open MySQL work bench
+8. Create a new database `eb_database`
+9. Right click on the database under the schemas in the left column and select Table Data Import Wizard
+![Instructions 1](images/Inst1.png)
+10. Select the file path to `purchase_details.csv` in the `pdf_to_csv` folder of this repo
+11. Follow the steps to import the csv file. Set the datetime format accordingly.
+12. Repeat the same steps for `redemption_details.csv`
+13. Run the following queries:
+```mysql
+use  eb_database;
+
+ALTER TABLE purchase_details
+RENAME COLUMN Prefix to `Purchase Prefix`;
+
+ALTER TABLE redemption_details
+RENAME COLUMN Prefix to `Redemption Prefix`;
+
+ALTER TABLE purchase_details
+RENAME COLUMN Denominations to `Purchase Denominations`;
+
+ALTER TABLE redemption_details
+RENAME COLUMN Denominations to `Redemption Denominations`;
+```
+14. The database is now set up.
+15. Inside of `db.yaml` back in VSCode, make sure details are correct according to the password set by you.
+16. Go to the terminal again and enter the following (this may defer for terminals that are not powershell):
+```bash
+$env:FLASK_APP = "app"
+```
+17. Run the app
+```bash
+python -m flask run
+```
+
+## Bonus Feature: Alluvial Diagram to show Flow of Money
+_chart has not rendered completely due to processing limitations_
+![Bonus Alluvial](images/bonusalluvial.jpeg)
--- app.py
+++ app.py
@@ -43,9 +43,20 @@ class CompanyForm(FlaskForm):
     company = StringField("Enter Company/Individual Name")
     submit = SubmitField()
 
+class PartyForm(FlaskForm):
+    party = StringField("Enter Name of Political Party")
+    submit = SubmitField()
+
 @app.route("/")
 def index():
-    return render_template("index.html")
+    cur = mysql.connection.cursor()
+    cur.execute("select `Name of the Purchaser`, `Name of the Political Party`, `Redemption Denominations` from purchase_details as pd right join redemption_details as rd on rd.`Bond Number`=pd.`Bond Number`;")
+    data = cur.fetchall()
+    table = []
+    for row in data:
+        table.append([row[0], row[1], int("".join(row[2].split(",")))])
+    cur.close()
+    return render_template("index.html", table=table)
 
 @app.route("/search", methods = ["GET", "POST"])
 def search():
@@ -62,6 +73,7 @@ def search():
         cur = mysql.connection.cursor()
         cur.execute("select " + ",".join(["`"+x+"`" for x in headings]) + f" from purchase_details as pd right join redemption_details as rd on rd.`Bond Number`=pd.`Bond Number` where pd.`Bond Number` = {bond_number};")
         table = cur.fetchall()
+        cur.close()
     return render_template("search.html", form=form, bond_number=bond_number, table=table, headings=headings)
 
 @app.route("/company_individual_stats", methods = ["GET", "POST"])
@@ -79,6 +91,7 @@ def company_individual_stats():
         cur = mysql.connection.cursor()
         cur.execute(f"select `Date of Purchase`, `Purchase Denominations` from purchase_details as pd right join redemption_details as rd on rd.`Bond Number`=pd.`Bond Number` where `Name of the Purchaser` = '{company.upper()}';")
         data = cur.fetchall()
+        cur.close()
         table = []
         data_dict = {}
         # year : (value, count)
@@ -96,6 +109,105 @@ def company_individual_stats():
         yaxis2 = [row[2] for row in table]
     return render_template("company_individual_stats.html", form=form, company=company, table=table, xaxis = xaxis, yaxis = yaxis, yaxis2=yaxis2)
 
-@app.route("/political_party_stats")
+@app.route("/political_party_stats", methods = ["GET", "POST"])
 def political_party_stats():
-    return render_template("political_party_stats.html")
+    form = PartyForm()
+    party = None
+    table = None
+    xaxis = None
+    yaxis = None
+    yaxis2 = None
+    if request.method == "POST":
+        party = form.party.data
+        form.party.data = None
+    if party:
+        cur = mysql.connection.cursor()
+        cur.execute(f"select `Date of Encashment`, `Redemption Denominations` from purchase_details as pd right join redemption_details as rd on rd.`Bond Number`=pd.`Bond Number` where `Name of the Political Party` = '{party.upper()}';")
+        data = cur.fetchall()
+        cur.close()
+        table = []
+        data_dict = {}
+        # year : (value, count)
+        for row in data:
+            if int(str(row[0])[:4]) in data_dict:
+                data_dict[int(str(row[0])[:4])][0] += int("".join(row[1].split(",")))
+                data_dict[int(str(row[0])[:4])][1] += 1
+            else:
+                data_dict[int(str(row[0])[:4])] = [int("".join(row[1].split(","))), 1]
+        for year in data_dict:
+            table.append((year, data_dict[year][0], data_dict[year][1]))
+        table = tuple(table)
+        xaxis = [row[0] for row in table]
+        yaxis = [row[1] for row in table]
+        yaxis2 = [row[2] for row in table]
+    return render_template("political_party_stats.html", form=form, party=party, table=table, xaxis = xaxis, yaxis = yaxis, yaxis2=yaxis2)
+
+@app.route("/party_donation_stats", methods = ["GET", "POST"])
+def party_donation_stats():
+    form = PartyForm()
+    party = None
+    table = None
+    total = None
+    xaxis = None
+    yaxis = None
+    if request.method == "POST":
+        party = form.party.data
+        form.party.data = None
+    if party:
+        cur = mysql.connection.cursor()
+        cur.execute(f"select `Name of the Purchaser`, `Purchase Denominations` from purchase_details as pd right join redemption_details as rd on rd.`Bond Number`=pd.`Bond Number` where `Name of the Political Party` = '{party.upper()}';")
+        data = cur.fetchall()
+        table = []
+        cur.close()
+        total = 0
+        xaxis = []
+        yaxis = []
+        donation = {}
+        for row in data:
+            if row[0] and row[1] is not None: # removing missing data
+                table.append((row[0], int("".join(row[1].split(",")))))
+                total += int("".join(row[1].split(",")))
+                if row[0] in donation:
+                    donation[row[0]]+=int("".join(row[1].split(",")))
+                else:
+                    donation[row[0]]=int("".join(row[1].split(",")))
+        for company in donation:
+            xaxis.append(company)
+            yaxis.append(donation[company])
+        table = tuple(table)
+    return render_template("party_donation_stats.html", form=form, party=party, table=table, total = total, xaxis=xaxis, yaxis=yaxis)
+
+@app.route("/company_donation_stats", methods = ["GET", "POST"])
+def company_donation_stats():
+    form = CompanyForm()
+    company = None
+    table = None
+    total = None
+    xaxis = None
+    yaxis = None
+    if request.method == "POST":
+        company = form.company.data
+        form.company.data = None
+    if company:
+        cur = mysql.connection.cursor()
+        cur.execute(f"select `Name of the Political Party`, `Redemption Denominations` from purchase_details as pd right join redemption_details as rd on rd.`Bond Number`=pd.`Bond Number` where `Name of the Purchaser` = '{company.upper()}';")
+        data = cur.fetchall()
+        table = []
+        cur.close()
+        total = 0
+        xaxis = []
+        yaxis = []
+        donations = {}
+        for row in data:
+            if row[0] and row[1] is not None: # removing missing data
+                table.append((row[0], int("".join(row[1].split(",")))))
+                total += int("".join(row[1].split(",")))
+                if row[0] in donations:
+                    donations[row[0]]+=int("".join(row[1].split(",")))
+                else:
+                    donations[row[0]]=int("".join(row[1].split(",")))
+        for party in donations:
+            xaxis.append(party)
+            yaxis.append(donations[party])
+        table = tuple(table)
+    return render_template("company_donation_stats.html", form=form, company=company, table=table, total = total, xaxis=xaxis, yaxis=yaxis)
